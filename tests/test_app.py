e1abf881c36c2c1b0a7339ce9fe016f2e03e08a9
Devair Luiz Moitim <dmoitim@gmail.com>
2025-05-13 22:03:07 -0300

    Commit dos fontes da aula 02

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from fastapi_zero.app import app
@@ -7,3 +9,11 @@ def test_root_deve_retornar_ola_mundo():
     client = TestClient(app)
     response = client.get('/')
     assert response.json() == {'message': 'Olá, mundo!'}
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_root_html_deve_retornar_ola_mundo_em_html():
+    client = TestClient(app)
+    response = client.get('/html')
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
