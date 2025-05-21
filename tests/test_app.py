1cd945a160b1424e6bf128daf21932e5bae5d9bd
Devair Luiz Moitim <dmoitim@gmail.com>
2025-05-20 22:04:39 -0300

    Commit dos fontes e quiz da aula 04

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,5 @@
 from http import HTTPStatus
 
-import pytest
-from fastapi.testclient import TestClient
-
-from fastapi_zero.app import app
-
-
-@pytest.fixture
-def client():
-    return TestClient(app)
-
 
 def test_root_deve_retornar_ola_mundo(client):
     response = client.get('/')
